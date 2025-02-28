982dda1914b8b7baa8e49bfe93ee4a9b27c989a1
giorgizena123 <zenaishvili621@gmail.com>
2025-02-28 17:50:13 +0400

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+class Book {
+    private String name;
+    private int numberOfPages;
 
-public class Book {
+    public Book(String name, int numberOfPages) {
+        this.name = name;
+        this.numberOfPages = numberOfPages;
+    }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public int getNumberOfPages() { return numberOfPages; }
+    public void setNumberOfPages(int numberOfPages) { this.numberOfPages = numberOfPages; }
+
+    @Override
+    public String toString() {
+        return "Book{name='" + name + "', numberOfPages=" + numberOfPages + "}";
+    }
 }
